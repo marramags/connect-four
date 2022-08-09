3a003d3a92a721e3723d890f1cb96efe08da1ee6
marramags <marra.emata@gmail.com>
2022-08-09 08:59:52 -0700

    gameboard array loop

--- js/main.js
+++ js/main.js
@@ -93,6 +93,17 @@ let testColumn = [5, 5, 5, 5, 5, 5, 5]
 const column0 = [[0,0],[0,1],[0,2],[0,3],[0,4],[0,5]]
 const column1 = [[1,0],[1,1],[1,2],[1,3],[1,4],[1,5]]
 
+// Boardgame array layout
+let gameBoardArr = [
+    [[0,0],[0,1],[0,2],[0,3],[0,4],[0,5]],
+    [[1,0],[1,1],[1,2],[1,3],[1,4],[1,5]],
+    [[2,0],[2,1],[2,2],[2,3],[2,4],[2,5]],
+    [[3,0],[3,1],[3,2],[3,3],[3,4],[3,5]],
+    [[4,0],[4,1],[4,2],[4,3],[4,4],[4,5]],
+    [[5,0],[5,1],[5,2],[5,3],[5,4],[5,5]],
+    [[6,0],[6,1],[6,2],[6,3],[6,4],[6,5]],
+    ]
+
 // let gameBoard = [
 //     '', '', '', '', '', '', '',
 //     '', '', '', '', '', '', '',
@@ -102,19 +113,11 @@ const column1 = [[1,0],[1,1],[1,2],[1,3],[1,4],[1,5]]
 //     '', '', '', '', '', '', '',
 // ]
 
-let gameBoard = [
-    null, null, null, null, null, null, null,
-    null, null, null, null, null, null, null,
-    null, null, null, null, null, null, null,
-    null, null, null, null, null, null, null,
-    null, null, null, null, null, null, null,
-    null, null, null, null, null, null, null,
-]
-
+let gameBoard;
 
 let player1 = [];
 let player2 = [];
-let currentPlayer;
+let currentPlayer = player1;
 
 let winner;
 let turns;
@@ -124,10 +127,55 @@ let message = document.querySelector('.message');
 const pointers = document.querySelectorAll('.pointers');
 const pointer0 = document.querySelector('.pointer-0');
 const pointer1 = document.querySelector('.pointer-1');
+const pointer2 = document.querySelector('.pointer-2');
+const pointer3 = document.querySelector('.pointer-3');
+const pointer4 = document.querySelector('.pointer-4');
+const pointer5 = document.querySelector('.pointer-5');
+const pointer6 = document.querySelector('.pointer-6');
 const allCircles = document.querySelectorAll('.all-circles');
-// console.log('pointer0');
+const restBtn = document.querySelector('.restart')
 
 // allCircles.setAttribute('style','background-color: red');
+
+// for (i =0 ; i < allCircles.length; i++) {
+//     allCircles[i].addEventListener('click', (e) => {
+//         console.log(`${e.target.parentElement.rowIndex},${e.target.cellIndex}`)
+//     })
+// }
+
+// for (i =0 ; i < allCircles.length; i++) {
+//     allCircles[i].addEventListener('click', (e) => {
+//         console.log(`${e.target.parentElement.rowIndex},${e.target.cellIndex}`)
+//     })
+// }
+
+function pointer0Click () {
+    gameBoardArr[0].reverse()
+    for (let i = 0; i<gameBoardArr[0].length; i++) {
+        if (gameBoardArr[0][i] === ) {
+            
+        }
+
+        // let testing = gameBoardArr[0][i]
+        // console.log(gameBoardArr[0][i]);
+        // testing.reverse()
+        // console.log(testing);
+    }
+}
+
+function pointer1Click () {
+    gameBoardArr[1].reverse()
+    for (let i = 0; i<gameBoardArr[1].length; i++) {
+        console.log(gameBoardArr[1][i]);
+    }
+}
+
+for (let i =0 ; i < allCircles.length; i++) {
+    allCircles[i].addEventListener('click', (e) => {
+        console.log(e.target)
+    })
+}
+
 
 startGame();
 
@@ -144,13 +192,13 @@ function startGame() {
     player2 = [];
     winner = null;
     let turns = player1;
-    currentPlayer = player1;
+    currentPlayer = true;
     const pointers = document.querySelectorAll('.pointers');
     const pointer0 = document.querySelector('.pointer-0');
 
 }
 
-// allCircles.addEventListener('click', playerMoves);
+allCircles.addEventListener('click', playerMoves);
 
 // pointer0.addEventListener('click', playerMoves);
 // pointer1.addEventListener('click', playerMoves);
@@ -165,6 +213,15 @@ function playerMoves() {
 
 }
 
+function playerTurns() {
+    if (currentPlayer === true) {
+        this.style.backgroundColor = red;
+    } else {
+        this.style.backgroundColor = yellow;
+    }
+    currentPlayer = !currentPlayer
+    playGame();
+}
 
 
 function restartGame() {
